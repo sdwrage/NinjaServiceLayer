89bc150f3493aef6df851e176fc2cb9899d6f0b4
Daniel Del Rio <ddelrio1986@gmail.com>
2013-05-11 13:50:45 -0400

    Add standard model methods

--- src/NinjaServiceLayer/Entity/AbstractEntity.php
+++ src/NinjaServiceLayer/Entity/AbstractEntity.php
@@ -4,8 +4,9 @@
  *
  * This class can be used as a base class for all entities.
  *
- * @author Daniel Del Rio <jesusfreakdelrio@gmail.com>
+ * @author Daniel Del Rio <ddelrio1986@gmail.com>
  * @package NinjaServiceLayer\Entity
+ * @filesource
  */
 
 namespace NinjaServiceLayer\Entity;
@@ -15,9 +16,83 @@ namespace NinjaServiceLayer\Entity;
  *
  * This class can be used as a base class for all entities.
  *
- * @author Daniel Del Rio <jesusfreakdelrio@gmail.com>
+ * @author Daniel Del Rio <ddelrio1986@gmail.com>
  * @package NinjaServiceLayer\Entity
  */
 class AbstractEntity
 {
+
+    /**
+     * Constructor
+     *
+     * Will store the provided options to properties if possible.
+     *
+     * @author Daniel Del Rio <ddelrio1986@gmail.com>
+     * @param null|array $options An array of options that should be stored to properties. The key should be the name of the property and the value should be the value to set the property to.
+     */
+    public function __construct($options = array())
+    {
+        $this->setOptions($options);
+    }
+
+    /**
+     * Magic Getter
+     *
+     * Allows you access a protected and private properties as if they were public. This ensures that the getter for the
+     * property is always used just incase anthing needs to be done like lazy-loading.
+     *
+     * @author Daniel Del Rio <ddelrio1986@gmail.com>
+     * @param string $propertyName The name of the property to get.
+     * @return mixed The value of the property.
+     */
+    public function __get($propertyName)
+    {
+        $methodName = 'get' . ucfirst($propertyName);
+        if (method_exists($this, $methodName)) {
+            return $this->$methodName();
+        } else {
+            throw new \Exception('Attempting to get an invalid property.');
+        }
+    }
+
+    /**
+     * Magic Setter
+     *
+     * Allows you to store values to the protected properties of this entity as if they were public. This will actually
+     * use a setter allowing us to perform any needed actions on the value before storing it.
+     *
+     * @author Daniel Del Rio <ddelrio1986@gmail.com>
+     * @param string $propertyName The name of the property to set.
+     * @param mixed $propertyValue The value to set to the property.
+     */
+    public function __set($propertyName, $propertyValue)
+    {
+        $methodName = 'set' . ucfirst($propertyName);
+        if (method_exists($this, $methodName)) {
+            $this->$methodName($propertyValue);
+            return $this;
+        } else {
+            throw new \Exception('Attempting to set an invalid property.');
+        }
+    }
+
+    /**
+     * Set Options
+     *
+     * Will store the provided options to properties if possible.
+     *
+     * @author Daniel Del Rio <ddelrio1986@gmail.com>
+     * @param null|array $options An array of options that should be stored to properties. The key should be the name of the property and the value should be the value to set the property to.
+     * @return self Returns itself to allow for method chaining.
+     */
+    public function setOptions($options = array())
+    {
+        foreach ($options as $propertyName => $propertyValue) {
+            $methodName = 'set' . ucfirst($propertyName);
+            if (method_exists($this, $methodName)) {
+                $this->$methodName($propertyValue);
+            }
+        }
+        return $this;
+    }
 }
