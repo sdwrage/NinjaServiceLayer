de2b3c0c5db03afb7d833c97bdb555ac067f8143
Daniel Del Rio <ddelrio1986@gmail.com>
2014-01-14 20:52:42 -0500

    Remove some comments

--- autoload_classmap.php
+++ autoload_classmap.php
@@ -1,12 +1,3 @@
 <?php
-/**
- * Classmap
- * 
- * This is the classmap for the module which can be used to setup autoloading.
- * 
- * @author Daniel Del Rio <ddelrio1986@gmail.com>
- * @package NinjaServiceLayer
- * @filesource
- */
 
 return array();
